2d0ab9786100befcb46cd87391bc5dce540fe5ef
Li Gang <vagabond1132@gmail.com>
2017-04-27 10:09:46 +0800

    Create sched_setaffinity.c
    绑定CPU核心数

---
+++ sched_setaffinity.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>   // -lm
+#include <sched.h>
+ 
+double waste_time(long n)
+{
+    double res = 0;
+    long double i = 0;
+    while(i <n * 200000) {
+        i++;
+        res += sqrt(i);
+    }
+    return res;
+}
+ 
+int main(int argc, char **argv)
+{
+    unsigned long mask = 1; /* processor 0 */
+ 
+    /* bind process to processor 0 */
+    if (sched_setaffinity(0, sizeof(mask), &mask) <0) { // int sched_setaffinity(pid_t pid, size_t cpusetsize, cpu_set_t *mask);
+        perror("sched_setaffinity");
+    }
+ 
+    /* waste some time so the work is visible with "top" */
+    printf ("result: %f\n", waste_time (200000));
+ 
+    mask = 2; /* process switches to processor 1 now */
+    if (sched_setaffinity(0, sizeof(mask), &mask) <0) { 
+        perror("sched_setaffinity");
+    }
+ 
+    /* waste some more time to see the processor switch */
+    printf ("result: %f\n", waste_time (20000));
+}
+
+
+//绑定CPU核心数
+top  摁1 即可查看cpu各个核心运转
+
